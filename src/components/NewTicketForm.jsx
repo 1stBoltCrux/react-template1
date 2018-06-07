25e3c382d299cea4821b5c3124279be5f60e8fe6
Student <example@example.com>
2018-06-07 14:53:15 -0700

    added form submit etc.

--- src/components/NewTicketForm.jsx
+++ src/components/NewTicketForm.jsx
@@ -1,20 +1,37 @@
 import React from 'react';
 
 function NewTicketForm(){
+  let _names = null;
+  let _location = null;
+  let _issue = null;
+
+  function handleNewTicketFormsSubmission(event) {
+    event.preventDefault();
+    console.log(_names);
+    console.log(_location);
+    console.log(_issue);
+    _names.value = '';
+    _location.value = '';
+    _issue.value = '';
+  }
   return (
     <div>
+      <form onSubmit={handleNewTicketFormsSubmission}></form>
       <form>
         <input
           type='text'
           id='names'
-          placeholder='Pair Names'/>
+          placeholder='Pair Names'
+        ref={(input) => {_names = input;}} />
         <input
           type='text'
           id='location'
-          placeholder='Location'/>
+          placeholder='Location'
+        ref={(input) => {_location = input;}}/>
         <textarea
           id='issue'
-          placeholder='Describe your issue.'/>
+          placeholder='Describe your issue.'
+        ref={(textarea) => {_issue = textarea;}}/>
         <button type='submit'>Help!</button>
       </form>
     </div>
